3ea576f02df0c7822abf76af17db737b0da4ba8b
Donovan Allen <dallen.928@gmail.com>
2025-08-28 15:55:47 -0400

    init dependencies

---
+++ packages/eslint-config/next.js
@@ -0,0 +1,39 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  env: {
+    node: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    ".*.js",
+    "node_modules/",
+    "dist/",
+  ],
+  overrides: [
+    {
+      files: ["*.js?(x)", "*.ts?(x)"],
+    },
+  ],
+};
